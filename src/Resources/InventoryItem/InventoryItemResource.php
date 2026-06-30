d7cc66ca8ac9c566a9bf08ee88d98c5ed5c8ceb4
jeffersongoncalves <gerson.simao.92@gmail.com>
2026-06-30 19:05:06 +0000

    P3: admin depth — stats widget, image upload, order infolist + action (v5)

--- src/Resources/InventoryItem/InventoryItemResource.php
+++ src/Resources/InventoryItem/InventoryItemResource.php
@@ -2,8 +2,8 @@
 
 namespace JeffersonGoncalves\FilamentCommerce\Inventory\Resources\InventoryItem;
 
-use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
 use Filament\Tables\Table;
 use JeffersonGoncalves\Commerce\Inventory\Models\InventoryItem;
 use JeffersonGoncalves\FilamentCommerce\Inventory\CommerceInventoryPlugin;
@@ -26,9 +26,9 @@ class InventoryItemResource extends Resource
         }
     }
 
-    public static function form(Form $form): Form
+    public static function form(Schema $schema): Schema
     {
-        return InventoryItemForm::configure($form);
+        return InventoryItemForm::configure($schema);
     }
 
     public static function table(Table $table): Table
